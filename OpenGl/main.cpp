3421ac7629c7f9051faf003d4fc99f1001ce1e79
uliadib12 <uliadib@gmail.com>
2021-11-27 01:26:43 +0700

    start engine

--- OpenGl/main.cpp
+++ OpenGl/main.cpp
@@ -1,10 +1,42 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
-
 using namespace std;
 
-int main() {
-	cout << "First Commit";
-	cin.get();
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
 
+int main() {
+
+	if (!glfwInit()) // Initialization
+	{
+		cout << "Error";
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	GLFWwindow* window = glfwCreateWindow(640, 480, "First OpenGl", NULL, NULL);
+	if (!window)
+	{
+		// Window or OpenGL context creation failed
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window); // Making the OpenGL context current
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		// Keep running
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
